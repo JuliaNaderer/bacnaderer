4ceff009f9db4a8ee74d6290bc63ad13baaeff0a
Flo <FlorianNaderer@protonmail.com>
2024-05-09 10:53:33 +0200

    Remove React Logo

--- src/App.js
+++ src/App.js
@@ -65,19 +65,6 @@ function App() {
           }}/>
           <button>Login User</button>
         </div>
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
