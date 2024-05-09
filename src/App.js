3728cc7f1e2d623fd8005cf31a2a90bc365cef53
Flo <FlorianNaderer@protonmail.com>
2024-05-09 10:40:04 +0200

    feature/InitialSetup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {createUserWithEmailAndPassword} from "firebase/auth";
+import {} from "firebase/auth";
+import {auth} from "./firebase";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  const register = async () => {
+  try{
+      const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword)
+      console.log(user);
+    }catch(error){
+      console.log(error.message)
+    }
+  }
+
+  const login = async () => {
+    try{
+      const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword)
+      console.log(user);
+    }catch(error){
+      console.log(error.message)
+    }
+
+  }
+
+  const logout = async () => {
+
+
+  }
+
+  
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+        <h3> Register User</h3>
+        <input placeholder = "Email"
+          onChange={(event) => {
+            setRegisterEmail(event.target.value);
+          }}/>
+          <input type='password' placeholder = "Password"
+          onChange={(event) => {
+            setRegisterPassword(event.target.value);
+          }}/>
+          <button onClick={register}>Create User</button>
+        </div>
+
+        <div>
+        <h3> Login User</h3>
+        <input placeholder = "Email"
+          onChange={(event) => {
+            setLoginEmail(event.target.value);
+          }}/>
+          <input type='password' placeholder = "Password"
+          onChange={(event) => {
+            setLoginPassword(event.target.value);
+          }}/>
+          <button>Login User</button>
+        </div>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCFfX6sh3fj05MVsfmTM36sy1OdR2AcJ-Y",
+  authDomain: "testpwa-3194a.firebaseapp.com",
+  projectId: "testpwa-3194a",
+  storageBucket: "testpwa-3194a.appspot.com",
+  messagingSenderId: "263410497873",
+  appId: "1:263410497873:web:215fd47d9324a052583b70"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
